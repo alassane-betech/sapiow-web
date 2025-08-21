6d2d6c5606cefe93c78fb7386ee01ce637e6e56d
moussaDevw <musadevweb@gmail.com>
2025-08-21 11:09:06 +0000

    comment

--- src/components/revenue/RevenueDisplay.tsx
+++ src/components/revenue/RevenueDisplay.tsx
@@ -5,6 +5,7 @@ interface RevenueDisplayProps {
 export default function RevenueDisplay({ amount }: RevenueDisplayProps) {
   return (
     <div className="space-y-6 mb-2 md:mb-0">
+      {/* TODO: Add currency */}
       <div className="text-6xl font-bold text-cobalt-blue">{amount}</div>
     </div>
   );
